46091317ca9f17246b3c912d591f95c7b1e708ea
Akash52 <ac8572611@gmail.com>
2022-03-13 22:30:36 +0530

    Sementic UI setup

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Message, Icon } from 'semantic-ui-react';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Message icon>
+        <Icon name='circle notched' loading />
+        <Message.Content>
+          <Message.Header>Just one second</Message.Header>
+          We're fetching that content for you.
+        </Message.Content>
+      </Message>
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../src/app/layout/index.css';
-import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App';
 
 ReactDOM.render(
