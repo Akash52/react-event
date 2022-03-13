58ffdbd563bdcc817a568891052d0071f5b41044
Akash52 <ac8572611@gmail.com>
2022-03-13 23:33:59 +0530

    Navigation bar & styling

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
 
 const App = () => {
   return (
     <>
-      <EventDashboard />
+      <NavBar />
+      <Container className='main'>
+        <EventDashboard />
+      </Container>
     </>
   );
 };
