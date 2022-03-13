54fdb51e42a20d469c705b7eccb55ac15233bc86
Akash52 <ac8572611@gmail.com>
2022-03-14 00:25:50 +0530

    Passing Props down to child components

--- src/features/events/eventDashboard/EventListAttende.jsx
+++ src/features/events/eventDashboard/EventListAttende.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { List, Image } from 'semantic-ui-react';
 
-const EventListAttende = () => {
+const EventListAttende = ({ attendee }) => {
   return (
     <List.Item>
-      <Image circular size='mini' src='/assets/user.png' alt='user' />
+      <Image circular size='mini' src={attendee.photoURL} alt='user' />
     </List.Item>
   );
 };
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,35 +2,35 @@ import React from 'react';
 import { Button, Icon, Item, List, Segment } from 'semantic-ui-react';
 import EventListAttende from './EventListAttende';
 
-const EventListItem = () => {
+const EventListItem = ({ event }) => {
   return (
     <Segment.Group>
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size='tiny' circular src='/assets/user.png' />
+            <Item.Image size='tiny' circular src={event.hostPhotoURL} />
             <Item.Content>
               <Item.Header content='Event Title' />
-              <Item.Description>Hosted By Bob</Item.Description>
+              <Item.Description>Hosted by {event.hostedBy}</Item.Description>
             </Item.Content>
           </Item>
         </Item.Group>
       </Segment>
       <Segment>
         <span>
-          <Icon name='clock' /> Date
-          <Icon name='marker' /> Venue
+          <Icon name='clock' /> {event.date}
+          <Icon name='marker' /> {event.venue}
         </span>
       </Segment>
       <Segment secondary>
         <List horizontal>
-          <EventListAttende />
-          <EventListAttende />
-          <EventListAttende />
+          {event.attendees.map((attendee) => (
+            <EventListAttende key={attendee.id} attendee={attendee} />
+          ))}
         </List>
       </Segment>
       <Segment clearing>
-        <div>Description of event</div>
+        <div>{event.description}</div>
         <Button color='teal' floated='right' content='View' />
       </Segment>
     </Segment.Group>
